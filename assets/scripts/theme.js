10e18276b22564a39c4a926134155101b1ea4bbf
Alexander Holiden Diakonov <holideny@gmail.com>
2020-09-14 21:55:43 +0700

    Стилизует библиотеку

---
+++ assets/scripts/theme.js
@@ -0,0 +1,3 @@
+document.querySelector('.Browser-tab--view').firstElementChild.innerHTML='HTML';
+document.querySelector('.Browser-tab--resources').firstElementChild.innerHTML='CSS и JS';
+document.querySelector('.Browser-tab--notes').firstElementChild.innerHTML='Заметки';
