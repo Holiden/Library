89d9be4ac46332da5624d30d2df8f1d207b8a66a
Alexander Holiden Diakonov <holideny@gmail.com>
2020-11-23 23:13:52 +0700

    Удалил gulp сборку

--- assets/scripts/theme.js
+++
@@ -1,3 +0,0 @@
-document.querySelector('.Browser-tab--view').firstElementChild.innerHTML='HTML';
-document.querySelector('.Browser-tab--resources').firstElementChild.innerHTML='CSS и JS';
-document.querySelector('.Browser-tab--notes').firstElementChild.innerHTML='Заметки';
